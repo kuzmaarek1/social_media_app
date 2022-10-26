6a628efa55ef8ab7f7683ef3683814f982a450de
arm650 <arm650>
2022-10-26 16:09:32 +0200

    Add login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Profile from './pages/Profile';
+import Auth from './pages/Auth';
 
 const App = () => {
   const styles = 'w-[22rem] h-56 bg-[#a6ddf0] rounded-[50%] blur-[64px] absolute';
@@ -7,7 +8,8 @@ const App = () => {
     <div className="overflow-hidden text-black bg-[#f3f3f3] p-4">
       <div className={`${styles} right-0 top-[-18%]`}></div>
       <div className={`${styles} left-[-8rem] top-[36%]`}></div>
-      <Profile />
+      {/*<Profile />*/}
+      <Auth />
     </div>
   );
 };
