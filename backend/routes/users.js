12d2f5e52e706885caebf70f0d88b8558e2faba0
arm650 <arm650>
2022-10-28 20:04:12 +0200

    Add User controller

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,15 @@
 import express from "express";
 const router = express.Router();
 
-import { signin, signup } from "../controllers/user.js";
+import { signin, signup,  getUser, updateUser, deleteUser, followUser,  unFollowUser } from "../controllers/user.js";
 
 router.post("/signin", signin);
 router.post("/signup", signup);
+
+router.get('/:id', getUser)
+router.put('/:id', updateUser)
+router.delete('/:id', deleteUser)
+router.put('/:id/follow', followUser)
+router.put('/:id/unfollow', unFollowUser)
 
 export default router;
